107895f173fdc306c8547f11b804bb32b55a2be1
Mikhail Shamanov <shamanovma@gmail.com>
2024-05-05 15:20:23 +0300

    Added null checks

--- src/main/java/com/mash/aoptracktime/repository/impl/InMemoryEmployeesRepository.java
+++ src/main/java/com/mash/aoptracktime/repository/impl/InMemoryEmployeesRepository.java
@@ -2,36 +2,47 @@ package com.mash.aoptracktime.repository.impl;
 
 import com.mash.aoptracktime.entity.Employee;
 import com.mash.aoptracktime.repository.EmployeesRepository;
+import io.micrometer.common.lang.NonNullApi;
 import org.springframework.stereotype.Repository;
 
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.StreamSupport;
 
 @Repository
+@NonNullApi
 public class InMemoryEmployeesRepository implements EmployeesRepository {
     private final Map<Long, Employee> data = new ConcurrentHashMap<>();
 
     @Override
     public <S extends Employee> S save(S entity) {
+        Objects.requireNonNull(entity);
+
         this.data.put(entity.getId(), entity);
         return entity;
     }
 
     @Override
     public <S extends Employee> Iterable<S> saveAll(Iterable<S> entities) {
+        Objects.requireNonNull(entities);
+
         entities.forEach(this::save);
         return entities;
     }
 
     @Override
     public Optional<Employee> findById(Long aLong) {
+        Objects.requireNonNull(aLong);
+
         return Optional.ofNullable(this.data.get(aLong));
     }
 
     @Override
     public boolean existsById(Long aLong) {
+        Objects.requireNonNull(aLong);
+
         return this.data.containsKey(aLong);
     }
 
@@ -42,6 +53,8 @@ public class InMemoryEmployeesRepository implements EmployeesRepository {
 
     @Override
     public Iterable<Employee> findAllById(Iterable<Long> longs) {
+        Objects.requireNonNull(longs);
+
         return this.data.values()
                 .stream()
                 .filter(employee -> StreamSupport.stream(longs.spliterator(), false)
@@ -56,21 +69,29 @@ public class InMemoryEmployeesRepository implements EmployeesRepository {
 
     @Override
     public void deleteById(Long aLong) {
+        Objects.requireNonNull(aLong);
+
         this.data.remove(aLong);
     }
 
     @Override
     public void delete(Employee entity) {
+        Objects.requireNonNull(entity);
+
         this.deleteById(entity.getId());
     }
 
     @Override
     public void deleteAllById(Iterable<? extends Long> longs) {
+        Objects.requireNonNull(longs);
+
         longs.forEach(this::deleteById);
     }
 
     @Override
     public void deleteAll(Iterable<? extends Employee> entities) {
+        Objects.requireNonNull(entities);
+
         entities.forEach(this::delete);
     }
 
