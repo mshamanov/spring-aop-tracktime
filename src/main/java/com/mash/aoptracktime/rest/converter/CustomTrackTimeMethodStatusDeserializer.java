531414964da651fd6054b79ce84804fe741fd61c
Mikhail Shamanov <shamanovma@gmail.com>
2024-05-05 16:32:22 +0300

    Added throwing exception if the format of enum is not valid

--- src/main/java/com/mash/aoptracktime/rest/converter/CustomTrackTimeMethodStatusDeserializer.java
+++ src/main/java/com/mash/aoptracktime/rest/converter/CustomTrackTimeMethodStatusDeserializer.java
@@ -1,18 +1,22 @@
 package com.mash.aoptracktime.rest.converter;
 
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.mash.aoptracktime.entity.TrackTimeMethodStatus;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 public class CustomTrackTimeMethodStatusDeserializer extends JsonDeserializer<TrackTimeMethodStatus> {
     public TrackTimeMethodStatus deserialize(JsonParser p, DeserializationContext context) throws IOException {
+        String valueAsString = p.getValueAsString();
         try {
-            return TrackTimeMethodStatus.valueOf(p.getValueAsString().toUpperCase());
+            return TrackTimeMethodStatus.valueOf(valueAsString.toUpperCase());
         } catch (IllegalArgumentException e) {
-            return TrackTimeMethodStatus.COMPLETED;
+            throw new JsonParseException(p, "Specified track time method status is invalid: " + valueAsString +
+                                            ", valid values: " + Arrays.toString(TrackTimeMethodStatus.values()));
         }
     }
 }
